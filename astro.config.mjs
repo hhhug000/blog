a00fe77aedb2c2c0307a43b8e364ce1d20983148
Hugo Haaxman <63868236+hhhug000@users.noreply.github.com>
2026-08-07 18:35:54 +0100

    Revert "Update astro.config.mjs"
    This reverts commit 5cccfae3a41c0f15eff9a4b0125610b07b6d78ce.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,13 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
+import keystatic from '@keystatic/astro';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://example.com',
     output: `static`,
-    integrations: [mdx(), sitemap(), react(), markdoc()],
+    integrations: [mdx(), sitemap(), react(), markdoc(), keystatic()],
     fonts: [
         {
             provider: fontProviders.local(),
