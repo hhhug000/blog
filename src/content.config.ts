d4a095b76450b48b0997d999fd6e344d08118420
Hugo Haaxman <63868236+hhhug000@users.noreply.github.com>
2026-08-08 10:15:57 +0100

    remove updatedate from schema

--- src/content.config.ts
+++ src/content.config.ts
@@ -12,7 +12,6 @@ const blog = defineCollection({
 			description: z.string(),
 			// Transform string to Date object
 			pubDate: z.coerce.date(),
-			updatedDate: z.coerce.date().optional(),
 			heroImage: z.optional(image()),
 			// Added fields for the new metadata layout
 			author: z.string().default('Hugo'),
